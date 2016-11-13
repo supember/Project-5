7c924bdd82d8036194bc7b072e03655981d4b208
hgong7 <hgong@vt.edu>
2016-11-13 18:35:16 -0500

    Update Student.java

--- src/prj5/Student.java
+++ src/prj5/Student.java
@@ -4,28 +4,51 @@ public class Student
 {
     private String major;
     private String hobby;
-    private String state;
+    private String region;
     private SinglyLinkedList<Boolean> responses;
-    
-    public Student(String major, String hobby, String state, SinglyLinkedList<Boolean> responses)
+    /**
+     * Creates a new Student object.
+     * 
+     * @param major of the Student
+     * @param hobby of the Student
+     * @param region of the Student
+     * @param responses of the Student
+     */
+    public Student(String major, String hobby, String region, SinglyLinkedList<Boolean> responses)
     {
         this.major = major;
         this.hobby = hobby;
-        this.state = state;
+        this.region = region;
         this.responses = responses;
     }
+    /**
+     * Returns the major of the Student.
+     * @return major of the Student
+     */
     public String getMajor()
     {
         return major;
     }
+    /**
+     * Returns the hobby of the Student.
+     * @return hobby of the Student
+     */
     public String getHobby()
     {
         return hobby;
     }
-    public String getState()
+    /**
+     * Returns the region of the Student. 
+     * @return region of the Student
+     */
+    public String getregion()
     {
-        return state;
+        return region;
     }
+    /**
+     * Returns the responses of the Student.
+     * @return responses of the Student
+     */
     public Boolean getResponses()
     {
         return responses;
