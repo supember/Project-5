a33cd58b249d4fcda9104c09a603cc7f0f59c5b7
kaliangvt <kaliang@vt.edu>
2016-11-13 19:07:55 -0500

    Add files via upload

---
+++ src/prj5/ProjectRunner.java
@@ -0,0 +1,16 @@
+package prj5;
+
+/**
+ * ProjectRunner runs MusicPreferenceVisualization.
+ * 
+ * @author Kali Liang (kaliang)
+ * @version 2016.11.13
+ */
+public class ProjectRunner
+{
+    public static void main(String[] args)
+    {
+        //Input input = new Input("MusicSurveyData2016F.csv");
+        new GUIWindow();
+    }
+}
